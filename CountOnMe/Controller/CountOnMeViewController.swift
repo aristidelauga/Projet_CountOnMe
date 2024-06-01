fc6c9ddecfca1f3210443bf44eaa7f586d429182
aristidelauga <aristide.lauga@gmail.com>
2024-06-01 23:59:38 +0200

    Logic being moved to Model

--- CountOnMe/Controller/CountOnMeViewController.swift
+++ CountOnMe/Controller/CountOnMeViewController.swift
@@ -11,49 +11,27 @@ import UIKit
 final class CountOnMeViewController: UIViewController {
 
 	// MARK: - Variables
-
+// TODO: Détecter quel bouton a ete tape dans le view controller puis décider dans le modele
     @IBOutlet weak private var textView: UITextView!
     @IBOutlet private var numberButtons: [UIButton]!
-
-
-    private var elements: [String] {
-        return textView.text.split(separator: " ").map { "\($0)" }
-    }
-    
-    // Error check computed variables
-	private var expressionIsCorrect: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "/" && elements.last != "*"
-    }
-    
-	private var expressionHaveEnoughElement: Bool {
-        return elements.count >= 3
-    }
-    
-	private var canAddOperator: Bool {
-		return elements.last != "+" && elements.last != "-" && elements.last != "/" && elements.last != "*"
-    }
-    
-	private var expressionHaveResult: Bool {
-        return textView.text.firstIndex(of: "=") != nil
-    }
+	let computationModel = ComputationModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-		textView.text = ""
+		self.computationModel.delegate = self
+		computationModel.cleanText()
     }
     
 	// MARK: - IBActions method
 
 	@IBAction func tappedACButton(_ sender: UIButton) {
-		textView.text = ""
+		computationModel.cleanText()
 	}
 
 	@IBAction func tappedDeleteButton(_ sender: UIButton) {
-		
-		if !textView.text.isEmpty {
-			textView.text.removeLast()
+		if !computationModel.expression.isEmpty {
+			computationModel.expression.removeLast()
 		}
-
 	}
 	
 
@@ -61,18 +39,11 @@ final class CountOnMeViewController: UIViewController {
         guard let numberText = sender.title(for: .normal) else {
             return
         }
-        
-        if expressionHaveResult {
-            textView.text = ""
-        }
-        
-        textView.text.append(numberText)
+		computationModel.tappedNumberButton(numberText: numberText)
     }
 
 	@IBAction func tappedDivisionButton(_ sender: UIButton) {
-		if canAddOperator {
-			textView.text.append(" / ")
-		} else {
+		computationModel.tappedDivisionButtonWithErrorMessage {
 			let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
 			alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
 			self.present(alertVC, animated: true, completion: nil)
@@ -80,121 +51,61 @@ final class CountOnMeViewController: UIViewController {
 	}
 
 	@IBAction func tappedMultiplicationButton(_ sender: UIButton) {
-		if canAddOperator {
-			textView.text.append(" * ")
-		} else {
+		computationModel.tappedMultiplicationButtonWithErrorMessage {
 			let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
 			alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
 			self.present(alertVC, animated: true, completion: nil)
+
 		}
 	}
 	
     @IBAction private func tappedAdditionButton(_ sender: UIButton) {
-        if canAddOperator {
-            textView.text.append(" + ")
-        } else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(alertVC, animated: true, completion: nil)
-        }
+		computationModel.tappedAdditionButtonWithErrorMessage {
+			let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+			alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+			self.present(alertVC, animated: true, completion: nil)
+		}
     }
     
     @IBAction private func tappedSubstractionButton(_ sender: UIButton) {
-        if canAddOperator {
-            textView.text.append(" - ")
-        } else {
-            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
-            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-            self.present(alertVC, animated: true, completion: nil)
-        }
+		computationModel.tappedSubstractionButtonWithErrorMessage {
+			let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+			alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+			self.present(alertVC, animated: true, completion: nil)
+		}
     }
 
     @IBAction private func tappedEqualButton(_ sender: UIButton) {
-        guard expressionIsCorrect else {
+		guard computationModel.expressionIsCorrect else {
             let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
         
-        guard expressionHaveEnoughElement else {
+		guard computationModel.expressionHaveEnoughElement else {
             let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
             alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             return self.present(alertVC, animated: true, completion: nil)
         }
-        
-         // Create local copy of operations
-		let operationsToReduce = elements
 
-		let postfixExpression = infixToPostfix(elements)
-		if let result = evaluatePostfix(postfixExpression) {
-			if !expressionHaveResult {
-				textView.text.append(" = \(result)")
-			}
-		} else {
+		computationModel.tappedEqualButtonWithErrorMessage {
 			self.present(UIAlertController(title: "Erreur", message: "Démarrez un nouveau calcul !", preferredStyle: .alert),
 						 animated: true,
 						 completion: nil)
 		}
-
-        if !expressionHaveResult {
-        	textView.text.append(" = \(operationsToReduce.first!)")
-        }
     }
 
 	func infixToPostfix(_ expression: [String]) -> [String] {
-
-		var output: [String] = []
-		var operandsStack: [String] = []
-
-		let precedence: [String: Int] = ["+": 1, "-": 1, "*": 2, "/": 2]
-
-		for element in expression {
-			// If the element is an integer, it's added to the output's array
-			if let _ = Int(element) {
-				output.append(element)
-			} else if let precedenceOfElement = precedence[element] {
-				while let last = operandsStack.last, let precedenceOfLast = precedence[last], precedenceOfLast >= precedenceOfElement {
-					// Removes the last element of `operandsStack` and adds it at the end of `output`
-					output.append(operandsStack.popLast()!)
-				}
-				// If the element is a sign symbol, it is added to the operandsStack
-				operandsStack.append(element)
-			}
-		}
-
-		// Removes every element of `operandsStack` one by one and add them to `output` at the same time
-		while let last = operandsStack.popLast() {
-			output.append(last)
-		}
-		return output
+		computationModel.infixToPostfix(expression)
 	}
 
 	func evaluatePostfix(_ expression: [String]) -> Int? {
-		var stack: [Int] = []
-
-		for element in expression {
-			if let number = Int(element) {
-				stack.append(number)
-			} else if element == "+" || element == "-" || element == "*" || element == "/" {
-				guard stack.count >= 2 else { return nil }
-				// Last element of the postfix expression
-				let right = stack.popLast()!
-				// Second last element of the postfix expression
-				let left = stack.popLast()!
-				var result: Int
-				switch element {
-					case "+": result = left + right
-					case "-": result = left - right
-					case "*": result = left * right
-					case "/": result = left / right
-					default: return nil
-				}
-				stack.append(result)
-			}
-		}
-		return stack.last
+		computationModel.evaluatePostFix(expression)
 	}
-
-
 }
 
+extension CountOnMeViewController: ComputationModelDelegate {
+	func getTextView(value: String) {
+		self.textView.text = value
+	}
+}
--- CountOnMe/Model/ComputationModel.swift
+++ CountOnMe/Model/ComputationModel.swift
@@ -8,6 +8,157 @@
 
 import Foundation
 
+protocol ComputationModelDelegate {
+	func getTextView(value: String)
+}
+
+
 final class ComputationModel {
 
+	var delegate: ComputationModelDelegate?
+
+	var expression = "" {
+		didSet {
+			delegate?.getTextView(value: expression)
+		}
+	}
+
+	var elements: [String] {
+		expression.split(separator: " ").map { "\($0)" }
+	}
+
+	// Error check computed variables
+	var expressionIsCorrect: Bool {
+		return self.elements.last != "+" && self.elements.last != "-" && self.elements.last != "/" && self.elements.last != "*"
+	}
+
+	var expressionHaveEnoughElement: Bool {
+		return self.elements.count >= 3
+	}
+
+	var canAddOperator: Bool {
+		return self.elements.last != "+" && self.elements.last != "-" && self.elements.last != "/" && self.elements.last != "*"
+	}
+
+	var expressionHaveResult: Bool {
+		return expression.firstIndex(of: "=") != nil
+	}
+
+	func cleanText() {
+		self.expression = ""
+	}
+
+	func tappedNumberButton(numberText: String) {
+		if expressionHaveResult {
+			cleanText()
+		}
+		expression.append(numberText)
+	}
+
+	func tappedDivisionButtonWithErrorMessage(_ UIHandler: () -> Void) {
+		if canAddOperator {
+			self.expression.append(" / ")
+		} else {
+			UIHandler()
+		}
+	}
+
+	func tappedMultiplicationButtonWithErrorMessage(_ UIHandler: () -> Void) {
+		if canAddOperator {
+			self.expression.append(" * ")
+		} else {
+			UIHandler()
+		}
+	}
+
+	func tappedAdditionButtonWithErrorMessage(_ UIHandler: () -> Void) {
+		if canAddOperator {
+			self.expression.append(" + ")
+		} else {
+			UIHandler()
+		}
+	}
+
+	func tappedSubstractionButtonWithErrorMessage(_ UIHandler: () -> Void) {
+		if canAddOperator {
+			self.expression.append(" - ")
+		} else {
+			UIHandler()
+		}
+	}
+
+	func tappedEqualButtonWithErrorMessage(_ UIHandler: () -> Void) {
+		// Create local copy of operations
+		let operationsToReduce = elements
+		let postfixExpression = infixToPostfix(elements)
+		
+		if let result = evaluatePostFix(postfixExpression) {
+			if !expressionHaveResult {
+				expression.append(" = \(result)")
+			} else { 
+				UIHandler()
+			}
+		}
+
+		if expressionHaveResult {
+			expression.append(" = \(operationsToReduce.first!)")
+		}
+	}
+
+	func infixToPostfix(_ expression: [String]) -> [String] {
+
+		var output: [String] = []
+		var operandsStack: [String] = []
+
+		let precedence: [String: Int] = ["+": 1, "-": 1, "*": 2, "/": 2]
+
+		for element in expression {
+			// If the element is an integer, it's added to the output's array
+
+			if let _ = Int(element) {
+				output.append(element)
+				//			if element is Int
+			} else if let precedenceOfElement = precedence[element] {
+				while let last = operandsStack.last, let precedenceOfLast = precedence[last], precedenceOfLast >= precedenceOfElement {
+					// Removes the last element of `operandsStack` and adds it at the end of `output`
+					output.append(operandsStack.popLast()!)
+				}
+				// If the element is a sign symbol, it is added to the operandsStack
+				operandsStack.append(element)
+			}
+		}
+
+
+		// Removes every element of `operandsStack` one by one and add them to `output` at the same time
+		while let last = operandsStack.popLast() {
+			output.append(last)
+		}
+		return output
+	}
+
+	func evaluatePostFix(_ expression: [String]) -> Int? {
+		var stack: [Int] = []
+
+		for element in expression {
+			if let number = Int(element) {
+				stack.append(number)
+			} else if element == "+" || element == "-" || element == "*" || element == "/" {
+				guard stack.count >= 2 else { return nil }
+				// Last element of the postfix expression
+				let right = stack.popLast()!
+				// Second last element of the postfix expression
+				let left = stack.popLast()!
+				var result: Int
+				switch element {
+					case "+": result = left + right
+					case "-": result = left - right
+					case "*": result = left * right
+					case "/": result = left / right
+					default: return nil
+				}
+				stack.append(result)
+			}
+		}
+		return stack.last
+	}
 }
